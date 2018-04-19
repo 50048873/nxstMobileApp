68975a601eb31bc9b5e02091b7460a1f15dc553b
nss <spallate@163.com>
2018-04-19 14:57:22 +0800

    人工填写增加数据提交前验证优化

---
+++ src/assets/js/regexp.js
@@ -0,0 +1,10 @@
+export default  {
+  errorBagName: 'errors', 
+  fieldsBagName: 'fields',
+  delay: 100,   
+  locale: 'zh_CN', 
+  strict: true,  
+  enableAutoClasses: true,
+  events: 'blur', 
+  inject: true
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@ import App from './App'
 import router from './router'
 import store from './store'
 import VueLazyloader from 'vue-lazyload'
+import regexpConfig from "@/assets/js/regexp";
+import VeeValidate, { Validator } from 'vee-validate';
+Vue.use(VeeValidate, regexpConfig); //一般插件都要use一下
 Vue.use(VueLazyloader, {
   loading: require('@/assets/img/imageLoading.gif'),
   error: require('@/assets/img/placeholder.png'),
   preLoad: 1.3,
   attempt: 1
 })
-
+Validator.extend('realNumber', {
+  getMessage:()=> {
+    return '输入数据有误！'
+  },
+  validate: value => {
+    // 获取输入的value
+   return /^([0-9])+(\.[0-9]+)?$/.test(value);
+  }
+});
 // 注册全局组件
 import NFooter from '@/components/base/NFooter'
 Vue.component('n-footer', NFooter)
