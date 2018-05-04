32e9e3ef857927ed9e26f590cf0d6330fa1c937d
nss <spallate@163.com>
2018-05-04 18:04:58 +0800

    添加组件测试

--- src/components/reservoirOverview/PatrolPath.vue
+++ src/components/reservoirOverview/PatrolPath.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="PatrolPath">
-    <h1>巡查轨迹测试页面</h1>
     <!-- <iframe style="width:100%;height:50%;" :src="this.getStaticPath('/static/ArcGisTest2.html')" frameborder="0"></iframe> -->
     <div id="viewDiv" class="balt-theme"></div>
   </div>
@@ -17,109 +16,71 @@ export default {
   },
   mixins: [getStaticPath],
   methods: {
-    init1() {
-      // const options = {
-      //   url: 'https://js.arcgis.com/4.6/'
-      // };
-      const options = {
-        url: 'http://localhost:8088/arcgis_js_api/library/4.6/dojo/dojo.js',
-        css: 'http://localhost:8088/arcgis_js_api/library/4.6/esri/css/main.css'
-      };
-      esriLoader.loadModules(['esri/views/MapView', 'esri/WebMap'], options)
-        .then(([MapView, WebMap]) => {
-          // then we load a web map from an id
-          var webmap = new WebMap({
-            portalItem: { // autocasts as new PortalItem()
-              id: 'f2e9b762544945f390ca4ac3671cfa72'
-            }
-          });
-          // and we show that map in a container w/ id #viewDiv
-          var view = new MapView({
-            map: webmap,
-            container: 'viewDiv'
-          });
-        })
-        .catch(err => {
-          // handle any errors
-          console.error(err);
-        });
-    },
-    init2() { 
+    init() { 
+
       esriLoader.loadModules([
         "esri/Map",
         "esri/views/MapView",
         "esri/widgets/BasemapToggle",
+        "esri/widgets/Legend",
+        "esri/layers/TileLayer",
         "esri/tasks/Locator",
         "dojo/domReady!"
-      ]).then(([Map, MapView, BasemapToggle, Locator]) => {
-        // Create the Map
+      ]).then(([Map,MapView, BasemapToggle,Legend,TileLayer, Locator]) => {
           var map = new Map({
             basemap: "hybrid"
           });
-
-          // Create the MapView and reference the Map in the instance
           var view = new MapView({
             container: "viewDiv",
             map: map,
             center: [114.372402,30.588834],
-            zoom: 17
+            zoom: 5
           });
-
-          // 1 - Create the widget
-          var toggle = new BasemapToggle({
-            // 2 - Set properties
-            view: view, // view that provides access to the map's 'topo' basemap
-            nextBasemap: "topo" // allows for toggling to the 'hybrid' basemap
-          });
-
-          // Add widget to the top right corner of the view
-          view.ui.add(toggle, "top-right");
-
-
-          // Set up a locator task using the world geocoding service
           var locatorTask = new Locator({
             url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
           });
+           view.on("click", function(event) {
+              event.stopPropagation();
+              // Get the coordinates of the click on the view
+              // around the decimals to 3 decimals
+              var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+              var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
 
-          /*******************************************************************
-           * This click event sets generic content on the popup not tied to
-           * a layer, graphic, or popupTemplate. The location of the point is
-           * used as input to a reverse geocode method and the resulting
-           * address is printed to the popup content.
-           *******************************************************************/
-          view.on("click", function(event) {
-            event.stopPropagation(); // overwrite default click-for-popup behavior
-
-            // Get the coordinates of the click on the view
-            var lat = Math.round(event.mapPoint.latitude * 1000000) / 1000000;
-            var lon = Math.round(event.mapPoint.longitude * 1000000) / 1000000;
-
-            view.popup.open({
-              // Set the popup's title to the coordinates of the location
-              title: "Reverse geocode: [" + lon + ", " + lat + "]",
-              location: event.mapPoint // Set the location of the popup to the clicked location
-            });
-
-            // Display the popup
-            // Execute a reverse geocode using the clicked location
-            locatorTask.locationToAddress(event.mapPoint).then(function(
-              response) {
-              // If an address is successfully found, show it in the popup's content
-              view.popup.content = response.address;
-            }).otherwise(function(err) {
-              // If the promise fails and no result is found, show a generic message
-              view.popup.content =
-                "No address was found for this location";
-            });
+              view.popup.open({
+                  // Set the popup's title to the coordinates of the clicked location
+                  title: "Reverse geocode: [" + lon + ", " + lat + "]",
+                  location: event.mapPoint // Set the location of the popup to the clicked location
+              });
           });
+
+          var legend = new Legend({
+            view: view
+          });
+          view.ui.add(legend, "top-right");
+          var toggle = new BasemapToggle({
+              map: map,
+              basemap: "hybird",
+              nextBasemap:"topo"
+          }, "BasemapToggle");
+          toggle.startup();
+          var transportationLyr = new TileLayer({
+            url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer",
+            id: "streets",
+            opacity: 0.7
+          });
+
+          var housingLyr = new TileLayer({
+            url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer",
+            id: "ny-housing"
+          });
+          view.ui.add(toggle, "top-right");
         }).catch(err => {
-          // handle any errors
           console.error(err);
         });
     }
   },
   mounted() {    
-    this.init1()
+    this.init()
   },
   created() {
 
@@ -134,6 +95,12 @@ export default {
     bottom: @footer-height;
     width: 100%;
     overflow-y: auto;
+    #BasemapToggle {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      z-index: 50;
+    }
   }
 </style>
 <style>
@@ -144,7 +111,7 @@ export default {
 }
 
 #viewDiv {
-  height: 60%;
+  height: 100%;
   width: 100%;
   background-color: lime;
 }
